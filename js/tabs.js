9eb5636f57efc8d5baacbd41ae86f1a4cbf05a73
Kishanthan Kingston <115884616+KishanthanKingston@users.noreply.github.com>
2026-02-13 15:20:56 +0100

    Dark_mode

--- js/tabs.js
+++ js/tabs.js
@@ -16,3 +16,25 @@ function openTab(tabId, element) {
         element.classList.add('active');
     }
 }
+
+// DARK MODE
+
+const toggleBtn = document.getElementById("theme-toggle");
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    toggleBtn.textContent = "☀️";
+}
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        toggleBtn.textContent = "☀️";
+    } else {
+        localStorage.setItem("theme", "light");
+        toggleBtn.textContent = "🌙";
+    }
+});
